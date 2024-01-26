0bbced42c03b4669692f68ae5d70bb05604fa484
Tom Wagg <tomjwagg@gmail.com>
2024-01-26 14:11:08 -0800

    add merging functionality to flares function

--- flair/flares.py
+++ flair/flares.py
@@ -3,6 +3,20 @@ import numpy as np
 
 
 def prep_stella(out_path='data'):
+    """Set up the Stella ConvNN object and download the stellar models.
+
+    Parameters
+    ----------
+    out_path : `str`, optional
+        Where to store the data, by default 'data'
+
+    Returns
+    -------
+    cnn : :class:`stella.ConvNN`
+        The Stella ConvNN object
+    models : :class:`list`
+        List of trained stellar models
+    """
     # set up stellar CNN
     cnn = stella.ConvNN(output_dir=out_path)
 
@@ -14,6 +28,22 @@ def prep_stella(out_path='data'):
 
 
 def get_stella_predictions(cnn, models, lc):
+    """Get the median flare probability across all models for each timestep in a lightcurve using Stella.
+
+    Parameters
+    ----------
+    cnn : :class:`stella.ConvNN`
+        The Stella ConvNN object
+    models : :class:`list`
+        List of trained stellar models
+    lc : :class:`lightkurve.lightcurve.TessLightCurve`
+        Lightcurve to predict on
+
+    Returns
+    -------
+    avg_pred : :class:`numpy.ndarray`
+        Median flare probability across all models for each timestep in the lightcurve
+    """
     # predict the flare probability for each model
     preds = np.zeros((len(models), len(lc)))
     for j, model in enumerate(models):
@@ -27,7 +57,7 @@ def get_stella_predictions(cnn, models, lc):
     return np.nanmedian(preds, axis=0)
 
 
-def get_flares(flare_prob, threshold=0.3, min_flare_points=3):
+def get_flares(flare_prob, threshold=0.3, min_flare_points=3, merge_absolute=2, merge_relative=0.2):
     """Get a mask for timesteps that are part of a flare and indices of flare starts and ends.
 
     Parameters
@@ -38,6 +68,10 @@ def get_flares(flare_prob, threshold=0.3, min_flare_points=3):
         Threshold probability, by default 0.3
     min_flare_points : `int`, optional
         Minimum number of contiguous timesteps necessary for a flare, by default 3
+    merge_absolute : `int`, optional
+        Merge flares that are closer than this number of timesteps, by default 2
+    merge_relative : `float`, optional
+        Merge flares that are closer than this fraction of the flare duration, by default 0.2
 
     Returns
     -------
@@ -52,10 +86,25 @@ def get_flares(flare_prob, threshold=0.3, min_flare_points=3):
     idx = np.where(flare_prob > threshold)[0]
 
     # split into contiguous blocks of indices
-    all_flares = np.split(idx, np.where(np.diff(idx) != 1)[0] + 1)
+    flares = np.split(idx, np.where(np.diff(idx) != 1)[0] + 1)
+
+    # merge blocks that are closer than merge_absolute or merge_relative
+    merged_flares = [flares[0]]
+    for i in range(1, len(flares)):
+        # define some variables for convenience
+        current_start, most_recent_end = flares[i][0], merged_flares[-1][-1]
+        most_recent_duration = most_recent_end - merged_flares[-1][0]
+        gap_size = current_start - most_recent_end
+
+        # merge if gap is small enough (either in absolute or relative terms)
+        if gap_size < merge_absolute or gap_size < merge_relative * most_recent_duration:
+            merged_flares[-1] = np.arange(merged_flares[-1][0], flares[i][-1] + 1, dtype=int)
+        # otherwise add the new flare to the list
+        else:
+            merged_flares.append(flares[i])
 
     # remove blocks with less than min_flare_points
-    long_enough_flares = [f for f in all_flares if len(f) >= min_flare_points]
+    long_enough_flares = [f for f in merged_flares if len(f) >= min_flare_points]
 
     # get indices of flare starts and ends
     flare_starts = [f[0] for f in long_enough_flares]
