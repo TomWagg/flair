8e0ef77a930a8a490a0c3e73f310c4394874feac
Tom Wagg <tomjwagg@gmail.com>
2024-01-26 14:23:49 -0800

    docstrings

--- flair/gp.py
+++ flair/gp.py
@@ -4,10 +4,26 @@ import numpy as np
 
 
 def fit_GP(lc, flare_mask):
+    """Fit a Gaussian Process to a lightcurve, ignoring flares.
+
+    Parameters
+    ----------
+    lc : :class:`lightkurve.lightcurve.TessLightCurve`
+        Lightcurve to fit
+    flare_mask : :class:`numpy.ndarray`
+        Boolean mask of flares in the lightcurve
+
+    Returns
+    -------
+    opt_gp : :class:`celerite2.GaussianProcess`
+        Optimized Gaussian Process
+    """
+    # mask the flares out of the lightcurve
     x = lc.time.value[~flare_mask]
     y = lc.flux.value[~flare_mask]
     y_err = lc.flux_err.value[~flare_mask]
 
+    # update the GP parameters
     def set_params(params, gp):
         gp.mean = params[0]
         theta = np.exp(params[1:])
@@ -17,10 +33,12 @@ def fit_GP(lc, flare_mask):
         gp.compute(x, diag=y_err**2 + theta[5], quiet=True)
         return gp
 
+    # calculate the negative log likelihood
     def neg_log_like(params, gp):
         gp = set_params(params, gp)
         return -gp.log_likelihood(y)
 
+    # initialize the GP and optimize
     gp = celerite2.GaussianProcess(mean=0.0, kernel=celerite2.terms.SHOTerm(sigma=1.0))
     initial_params = [0.0, 0.0, 0.0, np.log(10.0), 0.0, np.log(5.0), np.log(0.01)]
     set_params(initial_params, gp)
--- flair/lightcurve.py
+++ flair/lightcurve.py
@@ -2,7 +2,20 @@ import lightkurve as lk
 import numpy as np
 
 def get_all_lightcurves(**kwargs):
+    """Get all lightcurves matching the given search criteria.
+
+    Returns
+    -------
+    lcs : :class:`list`
+        List of :class:`lightkurve.lightcurve.TessLightCurve` objects
+
+    See Also
+    --------
+    :func:`lightkurve.search_lightcurve`
+    """
     search = lk.search_lightcurve(**kwargs)
+
+    # @TOBIN: Do we need this?
     search.table["dataURL"] = search.table["dataURI"]
 
     lcs = [search[i].download().PDCSAP_FLUX for i in range(len(search))]
