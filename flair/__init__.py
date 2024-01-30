014f500011688bda27fef73bbd428269f413069f
Tom Wagg <tomjwagg@gmail.com>
2024-01-30 13:44:54 -0800

    suppress tensorflow warnings

--- flair/__init__.py
+++ flair/__init__.py
@@ -1,1 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'            # suppress tensorflow warnings
 from . import flares, gp, inject, lightcurve, plot
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'            # re-enable tensorflow warnings
+del os
