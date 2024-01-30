8763892e42799d91afc7b1e043356e828becff90
Tom Wagg <tomjwagg@gmail.com>
2024-01-29 22:48:21 -0800

    docstrings and gp plot func

--- flair/plot.py
+++ flair/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 
 plt.rc('font', family='serif')
@@ -34,15 +35,40 @@ def plot_decorator(func):
             fig, ax = plt.subplots()
             kwargs['fig'] = fig
             kwargs['ax'] = ax
-        func(*args, **kwargs)
+        returned = func(*args, **kwargs)
         if 'show' in kwargs and kwargs['show'] or 'show' not in kwargs:
             plt.show()
-        return fig, ax
+        if returned is not None:
+            return (*returned, fig, ax) if isinstance(returned, tuple) else (returned, fig, ax)
+        else:
+            return fig, ax
     return wrapper
 
 
 @plot_decorator
-def plot_lc_with_stella(lc, avg_pred, fig=None, ax=None, show=True):
+def plot_lc_with_probs(lc, avg_pred, fig=None, ax=None, show=True):
+    """Plot a lightcurve with the flare probability as a colorbar.
+
+    Parameters
+    ----------
+    lc : :class:`lightkurve.lightcurve.TessLightCurve`
+        Lightcurve to plot
+    avg_pred : :class:`numpy.ndarray`
+        Flare probability for each timestep in the lightcurve
+    fig : :class:`matplotlib.figure.Figure`, optional
+        Figure to plot on, by default None (new figure created)
+    ax : :class:`matplotlib.axes.Axes`, optional
+        Axes to plot on, by default None (new axes created)
+    show : `bool`, optional
+        Whether to show the plot, by default True
+
+    Returns
+    -------
+    fig : :class:`matplotlib.figure.Figure`
+        Figure of plot
+    ax : :class:`matplotlib.axes.Axes`, optional
+        Axes of plot
+    """
     scatter = ax.scatter(lc.time.value, lc.flux.value, c=avg_pred,
                          vmin=0, vmax=1, label=f'Sector {lc.sector}')
     fig.colorbar(scatter, label="Flare Probability")
@@ -52,14 +78,81 @@ def plot_lc_with_stella(lc, avg_pred, fig=None, ax=None, show=True):
 
 
 @plot_decorator
-def plot_lc_and_gp(lc, flare_mask, gp=None, mu=None, variance=None, fig=None, ax=None, show=True):
+def plot_lc_and_gp(lc, flare_mask, flare_starts, flare_ends, gp,
+                   mu=None, variance=None, time_lims=None, highlight_flares=True,
+                   fig=None, ax=None, show=True):
+    """Plot a lightcurve with flares masked and the GP prediction.
 
+    Parameters
+    ----------
+    lc : :class:`lightkurve.lightcurve.TessLightCurve`
+        Lightcurve to plot
+    flare_mask : :class:`numpy.ndarray`
+        Boolean array with True for timesteps that are part of a flare
+    flare_starts : :class:`numpy.ndarray`
+        Indices of flare starts
+    flare_ends : :class:`numpy.ndarray`
+        Indices of flare ends
+    gp : :class:`celerite2.gp.GaussianProcess`
+        GP object
+    mu : :class:`numpy.ndarray`, optional
+        GP mean, by default None
+    variance : :class:`numpy.ndarray`, optional
+        GP variance, by default None
+    time_lims : `tuple`, optional
+        Time limits for the plot, by default None
+    highlight_flares : `bool`, optional
+        Whether to highlight flares on the plot, by default True
+    fig : :class:`matplotlib.figure.Figure`, optional
+        Figure to plot on, by default None (new figure created)
+    ax : :class:`matplotlib.axes.Axes`, optional
+        Axes to plot on, by default None (new axes created)
+    show : `bool`, optional
+        Whether to show the plot, by default True
+
+    Returns
+    -------
+    mu : :class:`numpy.ndarray`
+        GP mean
+    variance : :class:`numpy.ndarray`
+        GP variance
+    fig : :class:`matplotlib.figure.Figure`
+        Figure of plot
+    ax : :class:`matplotlib.axes.Axes`, optional
+        Axes of plot
+    """
+    # create a mask based on the time limits
+    if time_lims is not None:
+        time_mask = (lc.time.value > time_lims[0]) & (lc.time.value < time_lims[1])
+    else:
+        time_mask = np.ones_like(lc.time.value).astype(bool)
+        
+    # calculate the GP prediction if not provided
     if mu is None or variance is None:
-        mu, variance = gp.predict(y=lc.flux.value, t=x, return_var=True)
+        mu, variance = gp.predict(y=lc.flux.value[~flare_mask],
+                                  t=lc.time.value[time_mask],
+                                  return_var=True)
 
-    ax.plot(lc.time.value, lc.flux.value, label='Lightcurve')
-    ax.plot(lc.time.value[flare_mask], lc.flux.value[flare_mask], 'o', label='Flares')
-    ax.plot(lc.time.value, gp.predict(lc.flux.value, return_cov=False), label='GP')
+    # plot the lightcurve with flares masked
+    ax.plot(lc.time.value[~flare_mask & time_mask], lc.flux.value[~flare_mask & time_mask],
+            label="Data (flares masked)", alpha=1, color="C0")
 
-    ax.set(xlabel='Time [BJD-2457000]', ylabel='Normalized Flux')
-    ax.legend()
+    # plot the GP prediction
+    sigma = np.sqrt(variance)
+    ax.plot(lc.time.value[time_mask], mu, label="GP Prediction", color="C1")
+    ax.fill_between(lc.time.value[time_mask], mu - sigma, mu + sigma, color="C1", alpha=0.2)
+
+    # add a legend
+    ax.legend(fontsize=0.7 * fs)
+
+    # highlight flares if desired
+    if highlight_flares:
+        for s, e in zip(flare_starts, flare_ends):
+            start, end = lc.time.value[s], lc.time.value[e]
+            if time_lims is None or (start > time_lims[0] and end < time_lims[1]):
+                ax.axvspan(start, end, alpha=0.3, color="grey")
+
+    # set the axis labels
+    ax.set(xlabel="Time [BJD - 2457000, days]", ylabel="Flux [e-/s]")
+
+    return mu, variance
