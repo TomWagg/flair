1a07ffed4a16bd3e8aeeaac8f24bfef410113294
Tom Wagg <tomjwagg@gmail.com>
2024-01-30 18:08:42 -0800

    add lupita's code

--- flair/__init__.py
+++ flair/__init__.py
@@ -1,5 +1,5 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'            # suppress tensorflow warnings
-from . import flares, gp, inject, lightcurve, plot
+from . import flares, gp, inject, lightcurve, lupita, plot
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'            # re-enable tensorflow warnings
 del os
