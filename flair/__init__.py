23658df6373dc3c343d1bdd83ee895f58278194a
Tom Wagg <tomjwagg@gmail.com>
2024-04-11 11:48:33 -0700

    make it an actual package

--- flair/__init__.py
+++ flair/__init__.py
@@ -3,3 +3,4 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'            # suppress tensorflow warnin
 from . import flares, gp, inject, lightcurve, lupita, plot, ffd
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'            # re-enable tensorflow warnings
 del os
+from ._version import __version__
