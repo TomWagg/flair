575aefab0affe5b671a49a7b200e4fb569e5d5ad
Tom Wagg <tomjwagg@gmail.com>
2024-06-07 10:14:20 -0700

    fix slurm, run first 10

--- slurm/run_slurm.py
+++ slurm/run_slurm.py
@@ -1,12 +1,11 @@
 import os
-from copy import copy
 
 # read in the template slurm script
 with open("cvz.slurm") as f:
     template = f.read()
 
 n_stars = 0
-N_STAR_LIMIT = 1
+N_STAR_LIMIT = 10
 
 with open("../data/TESS_CVZ.csv") as f:
     # go through the CVZ file
@@ -17,7 +16,7 @@ with open("../data/TESS_CVZ.csv") as f:
         n_stars += 1
             
         # read in the star
-        tic, _, _, _, n_sector = f.readline().split(",")
+        tic, _, _, _, n_sector = line.split(",")
         tic = f"tic{tic}"
 
         sectors = [str(i) for i in range(int(n_sector)) if not os.path.exists(f"../output/{tic}_{i}.h5")]
@@ -25,7 +24,7 @@ with open("../data/TESS_CVZ.csv") as f:
         os.environ["FLAIR_TIC_ID"] = tic
                 
         # submit the job
-        os.system(f"sbatch --array={",".join(sectors)} cvz.slurm")
+        os.system(f"sbatch --array={','.join(sectors)} cvz.slurm")
 
         # break early for testing
         if n_stars >= N_STAR_LIMIT:
