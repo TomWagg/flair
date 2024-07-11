0d59fc75d1076bef613f22fb94ceb458d2044ad2
Tobin Wainer <tobinwainer@gmail.com>
2024-07-10 18:04:28 -0700

    Uploading Jim's FFD code to calculate the FFD

--- flair/__init__.py
+++ flair/__init__.py
@@ -1,6 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'            # suppress tensorflow warnings
-from . import flares, gp, inject, lightcurve, lupita, plot, ffd
+from . import flares, gp, inject, jim, lightcurve, lupita, plot
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'            # re-enable tensorflow warnings
 del os
 from ._version import __version__
