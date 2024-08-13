0b9ee303f3418bc85fdfaad5b7d248328a31948f
Tobin Wainer <tobinwainer@gmail.com>
2024-08-12 18:41:02 -0700

    Fully Working Pipeline

--- flair/gp.py
+++ flair/gp.py
@@ -28,15 +28,20 @@ def fit_GP(lc, flare_mask):
     def set_params(params, gp):
         gp.mean = params[0]
         theta = np.exp(params[1:])
+        print(theta)
+        if theta[1] <= 0.0 or theta[3] <= 0.0 or theta[4] <= 0.0 or np.isinf(theta[1]) or np.isinf(theta[3]) or np.isinf(theta[4]) or theta[1] >= 10**100 or theta[3] >= 10**100 or theta[4] >= 10**100:
+            return gp, False
         gp.kernel = celerite2.terms.SHOTerm(
             sigma=theta[0], rho=theta[1], tau=theta[2]
         ) + celerite2.terms.SHOTerm(sigma=theta[3], rho=theta[4], Q=0.25)
         gp.compute(x, diag=y_err**2 + theta[5], quiet=True)
-        return gp
+        return gp, True
 
     # calculate the negative log likelihood
     def neg_log_like(params, gp):
-        gp = set_params(params, gp)
+        gp, flag = set_params(params, gp)
+        if not flag:
+            return 1e10
         return -gp.log_likelihood(y)
 
     # initialize the GP and optimize
@@ -51,9 +56,9 @@ def fit_GP(lc, flare_mask):
     gp = celerite2.GaussianProcess(kernel, mean=0.0)
     gp.compute(x, yerr=y_err)
     
-    initial_params = [np.log(0.01), np.log(0.01), np.log(0.01), np.log(10.0), np.log(0.01), np.log(5.0), np.log(0.01)]
+    initial_params = [0, 0, 0, np.log(10.0), 0, np.log(5.0), np.log(0.01)]
     set_params(initial_params, gp)
     soln = minimize(neg_log_like, initial_params, method="L-BFGS-B", args=(gp,))
-    opt_gp = set_params(soln.x, gp)
+    opt_gp, _ = set_params(soln.x, gp)
     
     return opt_gp
--- slurm/run_slurm.py
+++ slurm/run_slurm.py
@@ -1,5 +1,9 @@
 import os
 
+# read in the template slurm script
+with open("cvz.slurm") as f:
+    template = f.read()
+
 n_stars = 0
 N_STAR_LIMIT = 1
 
@@ -8,16 +12,20 @@ with open("../data/TESS_CVZ.csv") as f:
     for line in f:
         if line.startswith("TICID"):
             continue
+
         n_stars += 1
-            
+
         # read in the star
         tic, _, _, _, n_sector = line.split(",")
         tic = f"tic{tic}"
+
         sectors = [str(i) for i in range(int(n_sector))]
+
         os.environ["FLAIR_TIC_ID"] = tic
-                
+
         # submit the job
         os.system(f"sbatch --array={','.join(sectors)} cvz.slurm")
+
         # break early for testing
         if n_stars >= N_STAR_LIMIT:
             break
