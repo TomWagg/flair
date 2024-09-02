a8c505bbea6c240832786865b9b48ccbbabc4d9a
Tobin Wainer <tobinwainer@gmail.com>
2024-09-02 14:56:42 -0700

    Add if statement to concatinating to make sure there are any flares

--- flair/flares.py
+++ flair/flares.py
@@ -125,8 +125,9 @@ def get_flares(flare_prob, threshold=0.3, min_flare_points=3, merge_absolute=2, 
 
     # create a mask for timesteps that are part of a flare
     flare_mask = np.zeros(len(flare_prob), dtype=bool)
-    flare_inds = np.concatenate(long_enough_flares)
-    flare_mask[flare_inds] = True
+    if len(long_enough_flares) > 1
+        flare_inds = np.concatenate(long_enough_flares)
+        flare_mask[flare_inds] = True
 
     return flare_mask, flare_starts, flare_ends
 
